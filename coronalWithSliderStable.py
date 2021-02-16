b6c01ccbbc913c747bfabd168ada34d3b4a78422
Swabahadine <swabmed@gmail.com>
2021-02-16 21:46:40 +0100

    add 3D rotation image feature

--- coronalWithSliderStable.py
+++ coronalWithSliderStable.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
 import sys
 import glob
-from scipy import ndimage
 
 np.set_printoptions(threshold=sys.maxsize)
 
@@ -36,6 +35,13 @@ print("skipped, no SliceLocation: {}".format(skipcount))
 # ensure they are in the correct order
 slices = sorted(slices, key=lambda s: s.SliceLocation)
 
+# pixel aspects, assuming all slices are the same
+ps = slices[0].PixelSpacing
+ss = slices[0].SliceThickness
+ax_aspect = ps[1]/ps[0]
+sag_aspect = ps[1]/ss
+cor_aspect = ss/ps[0]
+
 # create 3D array
 img_shape = list(slices[0].pixel_array.shape)
 img_shape.append(len(slices))
@@ -43,10 +49,9 @@ img_shape.append(len(slices))
 img3d = np.zeros(img_shape)
 
 # fill 3D array with the images from the files
-def img3DWithRotation(angle):
-    for i, s in enumerate(slices):
-        img2d = s.pixel_array
-        img3d[:, :, i] = ndimage.rotate(img2d, angle, reshape=False)
+for i, s in enumerate(slices):
+    img2d = s.pixel_array
+    img3d[:, :, i] = img2d
 
 
 def get_pixels_hu(image):
@@ -79,36 +84,34 @@ def coronalHuDepth(depth):
     coronal_hu = get_pixels_hu(coronal)
     return coronal_hu[::-1]
 
-img3DWithRotation(333)
 coronal_hu = coronalHuDepth(img_shape[0]//2)
-
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.1, right=0.9, top=0.83, bottom=0.1)
 
 
 axcolor = 'lightgoldenrodyellow'
-axAngle = plt.axes([0.25, 0.85, 0.65, 0.03], facecolor=axcolor)
+axMin = plt.axes([0.25, 0.9, 0.65, 0.03], facecolor=axcolor)
+axMax = plt.axes([0.25, 0.85, 0.65, 0.03], facecolor=axcolor)
 axDepth = plt.axes([0.25, 0.95, 0.65, 0.03], facecolor=axcolor)
-
-sAngle = Slider(axAngle, 'Angle', 0, 360, valinit=333, valstep=1)
-sDepth = Slider(axDepth, 'Depth', 230, 280, valinit=img_shape[0]//2, valstep=1)
-
-def updateAngle(val):
-    angleV = sAngle.val
-    angleV = sAngle.val
-    depthV = sDepth.val
-
-    img3DWithRotation(angleV)
-    ax.imshow(coronalHuDepth(depthV), cmap=plt.cm.gray, vmin=-120, vmax=1000)
+sMin = Slider(axMin, 'Min', -1000, 1000, valinit=-150, valstep=10)
+sMax = Slider(axMax, 'Max', -1000, 1000, valinit=250, valstep=10)
+sDepth = Slider(axDepth, 'Depth', 1, 512, valinit=img_shape[0]//2, valstep=1)
 
 def update(val):
+    minV = sMin.val
+    maxV = sMax.val
     depthV = sDepth.val
-    ax.imshow(coronalHuDepth(depthV), cmap=plt.cm.gray, vmin=-120, vmax=1000)
+    if (minV > maxV):
+        sMax.set_val(1000)
+        maxV = 1000
+    
+    ax.imshow(coronalHuDepth(depthV), cmap=plt.cm.gray, vmin=minV, vmax=maxV)
 
-sAngle.on_changed(updateAngle)
+sMin.on_changed(update)
+sMax.on_changed(update)
 sDepth.on_changed(update)
 
-ax.imshow(coronal_hu, cmap=plt.cm.gray, vmin=-120, vmax=1000)
+ax.imshow(coronal_hu, cmap=plt.cm.gray, vmin=-150, vmax=250)
 plt.show()
 
 
