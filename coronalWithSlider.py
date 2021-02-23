b3cb3d8a5ce76716f64a08e2179751aaaf702b3c
Swabahadine <swabmed@gmail.com>
2021-02-23 20:38:01 +0100

    rotation avec changement de repère

--- coronalWithSlider.py
+++ coronalWithSlider.py
@@ -5,6 +5,7 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 import sys
 import glob
 from scipy import ndimage
+import math
 
 np.set_printoptions(threshold=sys.maxsize)
 
@@ -40,14 +41,32 @@ slices = sorted(slices, key=lambda s: s.SliceLocation)
 img_shape = list(slices[0].pixel_array.shape)
 img_shape.append(len(slices))
 
-img3d = np.zeros(img_shape)
+def rotateX(originX, originY, x, y, angleD):
+    angle = angleD * math.pi / 180
+    if (originY <= y):
+        cos = math.cos(angle) * (y - originY)
+        sin = math.sin(angle) * (y - originY)
+        return cos + originY, sin + originX
+    else:
+        cos = math.cos(angle + math.pi) * (originY - y)
+        sin = math.sin(angle + math.pi) * (originY - y)
+        return cos + originY, sin + originX
 
 # fill 3D array with the images from the files
-def img3DWithRotation(angle):
+def img3DWithRotation(angle, depth, depthY = 256):
+    imgFinal = []
     for i, s in enumerate(slices):
         img2d = s.pixel_array
-        img3d[:, :, i] = ndimage.rotate(img2d, angle, reshape=False)
-
+        rowArray = []
+        for j in range(len(img2d)):
+            cos, sin = rotateX(depth, depthY, depth, j, angle)
+            if (math.ceil(sin) >= 512 or math.ceil(cos) >=512):
+                rowArray.append(0)
+            else:
+                rowArray.append(img2d[math.ceil(sin)][math.ceil(cos)])
+        imgFinal.append(rowArray)
+        # img3d[:, :, i] = ndimage.rotate(img2d, angle, reshape=False)
+    return imgFinal
 
 def get_pixels_hu(image):
     #image = np.stack([s.pixel_array for s in slices])
@@ -74,13 +93,13 @@ def get_pixels_hu(image):
     return np.array(image, dtype=np.int16)
 
 # plot 3 orthogonal slices
-def coronalHuDepth(depth):
-    coronal = img3d[depth, :, :].T
+def coronalHuDepth(angle, depth):
+    imgFinal = img3DWithRotation(angle, depth)
+    coronal = np.array(imgFinal) #img3d[depth, :, :].T
     coronal_hu = get_pixels_hu(coronal)
     return coronal_hu[::-1]
 
-img3DWithRotation(333)
-coronal_hu = coronalHuDepth(img_shape[0]//2)
+coronal_hu = coronalHuDepth(0, img_shape[0]//2)
 
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.1, right=0.9, top=0.83, bottom=0.1)
@@ -90,22 +109,22 @@ axcolor = 'lightgoldenrodyellow'
 axAngle = plt.axes([0.25, 0.85, 0.65, 0.03], facecolor=axcolor)
 axDepth = plt.axes([0.25, 0.95, 0.65, 0.03], facecolor=axcolor)
 
-sAngle = Slider(axAngle, 'Angle', 0, 360, valinit=333, valstep=1)
-sDepth = Slider(axDepth, 'Depth', 230, 280, valinit=img_shape[0]//2, valstep=1)
+sAngle = Slider(axAngle, 'Angle', 0, 360, valinit=0, valstep=1)
+sDepth = Slider(axDepth, 'Depth', 150, 350, valinit=img_shape[0]//2, valstep=1)
 
 def updateAngle(val):
     angleV = sAngle.val
     angleV = sAngle.val
     depthV = sDepth.val
 
-    img3DWithRotation(angleV)
-    ax.imshow(coronalHuDepth(depthV), cmap=plt.cm.gray, vmin=-120, vmax=1000)
+    ax.imshow(coronalHuDepth(angleV, depthV), cmap=plt.cm.gray, vmin=-120, vmax=1000)
 
 def update(val):
     depthV = sDepth.val
-    ax.imshow(coronalHuDepth(depthV), cmap=plt.cm.gray, vmin=-120, vmax=1000)
+    angleV = sAngle.val
+    ax.imshow(coronalHuDepth(angleV, depthV), cmap=plt.cm.gray, vmin=-120, vmax=1000)
 
-sAngle.on_changed(updateAngle)
+sAngle.on_changed(update)
 sDepth.on_changed(update)
 
 ax.imshow(coronal_hu, cmap=plt.cm.gray, vmin=-120, vmax=1000)
